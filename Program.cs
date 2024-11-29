42dfbfb1d57e5447ea66f6a657fc7e1b0a557b43
Bl1nker <Alex_001@sibmail.com>
2024-11-29 15:02:45 +0700

    Task 2 done

--- Program.cs
+++ Program.cs
@@ -25,14 +25,57 @@ class HeapSortTasks
             Heapify(tasks, tasks.Length, i);
         }
         for (int i = tasks.Length - 1; i >= 0; i--)
-        {            
+        {
             Heapify(tasks, i, 0);
         }
     }
     public static void Main()
     {
-        int[] tasks = [3, 1, 4, 2, 5];
-        SortTasksByPriority(tasks);
-        Console.WriteLine(JsonSerializer.Serialize(tasks));
+        // Task 1
+        // int[] tasks = [3, 1, 4, 2, 5];
+        // SortTasksByPriority(tasks);
+        // Console.WriteLine(JsonSerializer.Serialize(tasks));
+
+        // Task 2
+        long[] phoneNumbers = [9876543210L, 1234567890L, 5555555555L, 1000000000L];
+        SortPhoneNumbers(phoneNumbers);
+        Console.WriteLine(JsonSerializer.Serialize(phoneNumbers));
+    }
+
+    private static void CountingSort(long[] arr, int exp)
+    {        
+        long[] output = new long[arr.Length];
+        int[] count = new int[10]; // 10 - количество разрядов в номере телефона
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int tmp = (int)(arr[i] / exp) % 10;
+            count[tmp]++;
+        }
+
+        for (int i = 1; i < 10; i++)
+        {
+            count[i] += count[i - 1];
+        }
+
+        for(int i = arr.Length -1; i >= 0; i--)
+        {
+            int tmp = (int)(arr[i] / exp) % 10;
+            output[count[tmp] - 1] = arr[i];
+            count[tmp]--;
+        }
+
+        for( int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = output[i];
+        }
+    }
+    public static void SortPhoneNumbers(long[] arr)
+    {
+        long max = arr.Max();
+
+        for(int exp = 1; max / exp > 0; exp *= 10)
+        {
+            CountingSort(arr, exp);
+        }
     }
 }
